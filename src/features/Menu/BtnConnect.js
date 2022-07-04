0f8b8ee77a372451efd61e7177b36c146b4aa276
Marchetti Matteo <=>
2022-07-04 16:26:16 +0200

    si torna sui propri passi....

--- src/features/Menu/BtnConnect.js
+++ src/features/Menu/BtnConnect.js
@@ -1,29 +1,42 @@
 import { useMoralis } from "react-moralis";
-import React, { useEffect, useState, } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { store } from '../../app/store';
-import { logHandler, useMenu } from './menuSlice';
 
 export default function BtnConnect({ id, names, href, img, classes, current }) {
-    const menu = useSelector(useMenu)
-    const dispatch = useDispatch();
-    const { isAuthenticated, authenticate, logout } = useMoralis();
-
+    const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
+    
     useEffect(() => {
       if (isAuthenticated) {
         document.getElementById('btn-connect-header').innerHTML=names[1];
-        console.log(store.getState())
       } else { 
         document.getElementById('btn-connect-header').innerHTML=names[0];
-        console.log(store.getState())
-        dispatch(logHandler({isAuthenticated})).then(console.log(store.getState()));
-        
       }
     }, [isAuthenticated]);
 
+    const toggleLogin = async () => {
+      if (!isAuthenticated) {
+        await authenticate({signingMessage: "Benvenuto in chessboard.io" })
+          .then(function (_user) {
+            console.log("logged in _user:", _user);
+            console.log(_user.get("ethAddress"));
+            console.log(store.getState());
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      } else { await logOut(); }
+    }
+
+    const logOut = async () => {
+      await logout();
+      console.log(store.getState());
+      console.log("logged out");
+    }
+
     return (
         <div className={"Btn-Connect inline-block mr-2 mt-2 bg-red-400 p-1 text-gray-200 rounded border-2 border-solid border-red-600 mb-2"+classes}>
-            <button id="btn-connect-header" className="Connect-Link" onClick={()=>{ dispatch(logHandler()).then(console.log(store.getState())); }} >{names[0]}</button>
+            <button id="btn-connect-header" className="Connect-Link" onClick={()=>{ toggleLogin(); }} >{names[0]}</button>
             <img className="" src={img}></img>
         </div>
     );
--- src/features/Menu/menuAPI.js
+++ src/features/Menu/menuAPI.js
@@ -1,4 +1,4 @@
 import { useMoralis } from "react-moralis";
 
-export const login = async () =>{ const {authenticate}=useMoralis();await authenticate() }
-export const logout = async () =>{ const {logout}=useMoralis();await logout() }
+export const login = async ()=>{const {authenticate}=useMoralis(); await authenticate()}
+export const logout = async ()=>{const {logout}=useMoralis(); await logout()}
