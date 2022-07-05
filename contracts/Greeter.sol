010ed1a9d795f2174d880d7e96160e71b2f3f881
Marchetti Matteo <=>
2022-07-05 16:33:13 +0200

    è un casino...

---
+++ contracts/Greeter.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.13;
+contract Greeter{
+    
+    receive() external payable {
+  
+    }
+} 
--- migrations/2_deploy_chessboard.js
+++ migrations/2_deploy_chessboard.js
@@ -1,5 +1,5 @@
-const ChessBoard = artifacts.require("ChessBoard");
+const Greeter = artifacts.require("Greeter");
 
 module.exports = function (deployer) {
-  deployer.deploy(ChessBoard, '0xC817494c318EDf654C0ee0Ee8294f53B6f4878fa', '0x92898dC274F6BBc0e9C94d1Cf6387e05bA03B711');
+  deployer.deploy(Greeter, '0xC817494c318EDf654C0ee0Ee8294f53B6f4878fa', '0x92898dC274F6BBc0e9C94d1Cf6387e05bA03B711');
 };
